295aacb22fd18477b80bce0f8c7f19b18899646a
Sûrya KARUNAGODA <surya@effi-net.com>
2014-09-19 11:04:07 +0200

    Removed the \n added to every output lines

--- lib/simple_html_dom.php
+++ lib/simple_html_dom.php
@@ -420,7 +420,7 @@ class simple_html_dom_node
 		// render end tag
 		if (isset($this->_[HDOM_INFO_END]) && $this->_[HDOM_INFO_END]!=0)
 			$ret .= '</'.$this->tag.'>';
-		return $ret."\n";
+		return $ret;
 	}
 
 	// get dom node's plain text
